b106cedc6b568412da81c1f151ec48d937f0325d
dna737 <adnanhere27@gmail.com>
2023-06-07 12:15:54 -0500

    implement fetchCurrentPlayer() and use game.validateInput() to restrict double click

--- script.js
+++ script.js
@@ -15,24 +15,29 @@ const game = (() => {
   let board = new Array(9).fill("");
   const playerOne = createPlayer(1, "X");
   const playerTwo = createPlayer(2, "O");
-  let clicks = 0; //These clicks will be useful to identify the current player.
-  const makeMove = function (currPlayer, index) {
+  let clicks = 0; //These clicks act like a binary switch to identify the current player.
+
+  const fetchCurrentPlayer = function (index) {
+    let currPlayer = null;
+    if (index % 2 === 0) {
+      currPlayer = playerOne;
+    } else {
+      currPlayer = playerTwo;
+    }
+    return currPlayer;
+  };
+
+  const makeMove = function (index) {
+    let currPlayer = fetchCurrentPlayer(index);
     board[index] = currPlayer.char;
     //TODO: make it more restrictive later.
     console.log("currPlayer:", currPlayer, "\t index:", index);
     //TODO: create another function that displays the result on the screen ("X" mark after the first click, etc.), and
     //then implement the validators.
     //NOTE: the code below is a rough layout of what needs to be implemented later in this method.
-    /*
-    let currPlayer = null;
-    if(clicks % 2 == 0){
-        currPlayer = playerOne;
-    }else{
-        currPlayer = playerTwo;
-    }
+    //do stuff with currPlayer's attributes.
 
-    do stuff with currPlayer's attributes.
-    */
+    board[index] = currPlayer.char; //esdf continue from here.
   };
   const validateInput = function (index) {
     return board[index] === "";
@@ -59,13 +64,20 @@ function activateBoxes() {
       console.log(itemIndex);
       console.log(game.validateInput(itemIndex));
       //FIXME: add an if-else statement. If the move is correct, then call the makeMove function; otherwise, call the displayInvalidInput() function.
-      ++game.clicks;
+
+      if (game.validateInput(itemIndex)) {
+        game.makeMove(itemIndex);
+        ++game.clicks;
+      } else {
+        console.log("reclicked on an old grid item!");
+      }
     });
   });
 }
 
 activateBoxes();
 
+//TODO: get rid of this function as it's only for debugging purposes.
 function displayGame() {
   console.log(game.board);
 }

af1b3785f48de167728315739dedb0524c1978c1
dna737 <adnanhere27@gmail.com>
2023-06-10 12:32:57 -0500

    remove completed TODOs and unnecessary console.logs

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-/*
-1. know what player's turn it is
-2. know what character belongs to a player
-    3. identify the winning criteria and draw criteria (it's okay if it ends after all the slots have been occupied)
-    4. score alterations should be private, so we should be using closures for this (IIFE).
-    5. the only functions that could be public are the ones that make a move (for example: makeMove(player))
-    
-    */
-
 const createPlayer = (num, char) => {
   return { num, char };
 };
@@ -31,7 +22,6 @@ const game = (() => {
   const makeMove = function (index) {
     let currPlayer = fetchCurrentPlayer();
     board[index] = currPlayer.char;
-    //TODO: make it more restrictive later.
     console.log("currPlayer:", currPlayer, "\t index:", index);
     //NOTE: the code below is a rough layout of what needs to be implemented later in this method.
 
@@ -42,8 +32,6 @@ const game = (() => {
   };
 
   const placePlayerMarker = function (index, gridItem) {
-    console.log("hi from the placePlayerMarker methdo! :)");
-    console.log("gridItem", gridItem);
     let marker = document.createElement("h1");
     marker.textContent = fetchCurrentPlayer(index).char;
     marker.classList.add("white-text", "marker");
@@ -61,7 +49,6 @@ const game = (() => {
   };
 
   const checkDiagonals = function () {
-    console.log("checkDiagonals");
     let decreasingDiagonal =
       board[0] !== "" && board[0] === board[4] && board[4] === board[8];
     let increasingDiagonal =
@@ -76,7 +63,6 @@ const game = (() => {
   };
 
   const checkVerticals = function () {
-    console.log("checkVerticals");
     let leftVertical =
       board[0] !== "" && board[3] === board[0] && board[6] === board[0];
     let midVertical =
@@ -96,17 +82,12 @@ const game = (() => {
   };
 
   const checkHorizontals = function () {
-    console.log("checkHorizontals");
     let topHorizontal =
       board[0] !== "" && board[0] === board[1] && board[0] === board[2];
     let midHorizontal =
       board[3] !== "" && board[3] === board[4] && board[3] === board[5];
     let bottomHorizontal =
       board[6] !== "" && board[6] === board[7] && board[6] === board[8];
-
-    if (topHorizontal || midHorizontal || bottomHorizontal) {
-      console.log("winnerViaHorizontal!");
-    }
 
     if (topHorizontal) {
       displayVerdict(board[0]);
@@ -145,16 +126,6 @@ function activateBoxes() {
         box.classList[box.classList.length - 1].substring(5)
       ); //fetches "item-0".."item-8"
 
-      /*FIXME: 
-        1. use the itemIndex's value to update game.board
-        2. add <p> to the clicked box. The text content can be anything for now.
-        3. after step 2, replace it with either "X" or "O".
-      4. After that, think about the switching of "X" and "O" after every turn.
-      */
-
-      console.log(itemIndex);
-      console.log(game.validateInput(itemIndex));
-
       if (game.validateInput(itemIndex)) {
         game.makeMove(itemIndex);
         game.placePlayerMarker(itemIndex, box);
@@ -164,7 +135,6 @@ function activateBoxes() {
           }, "1000");
           deactivateBoxes();
         }
-        console.log("game clicks just incremented!");
         ++game.clicks;
       } else {
         alert("Please select another grid item.");
@@ -186,7 +156,3 @@ function restartGame() {
 }
 
 activateBoxes();
-
-//TODO: be wary of replacing the current item in the array once the user clicks the grid item. Once a box has been used, it should be irreplaceable.
-
-displayGame();

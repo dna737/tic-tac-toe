a7fcbf14c018af8e0dbf4b7c9d87af850e292d32
dna737 <adnanhere27@gmail.com>
2023-06-10 12:27:58 -0500

    get rid of displayBoard()

--- script.js
+++ script.js
@@ -187,11 +187,6 @@ function restartGame() {
 
 activateBoxes();
 
-//TODO: get rid of this function as it's only for debugging purposes.
-function displayGame() {
-  console.log(game.board);
-}
-
 //TODO: be wary of replacing the current item in the array once the user clicks the grid item. Once a box has been used, it should be irreplaceable.
 
 displayGame();
